756f1f9258b4a29aa460c011d001e4380f3e65a8
albact4 <albaclosat@gmail.com>
2023-09-04 13:55:00 -0400

    Buttons fix

---
+++ index.js
@@ -0,0 +1,39 @@
+
+
+
+/*Buttons code*/
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    // Get the buttons by their IDs
+    const learnButton = document.getElementById("learnButton");
+    const redirectButton = document.getElementById("redirectButton");
+    const explanationButton = document.getElementById("explanationButton");
+    const redirectButton1 = document.getElementById("redirectButton1");
+    const explanationButton1 = document.getElementById("explanationButton1");
+  
+    learnButton.addEventListener('click', () => {
+      // Redirect to the desired HTML file
+      window.location.href = 'about.html';
+    });
+    // Add click event listeners to the buttons
+    redirectButton.addEventListener("click", () => {
+      // Redirect to the desired page when the button is clicked
+      window.location.href = "GoogleMaps.html";
+    });
+    redirectButton1.addEventListener("click", () => {
+      // Redirect to the desired page when the button is clicked
+      window.location.href = "hazard.html";
+    });
+  
+    explanationButton.addEventListener("click", () => {
+      // Redirect to another page when the button is clicked
+      window.location.href = "about.html";
+    });
+    explanationButton1.addEventListener("click", () => {
+      // Redirect to another page when the button is clicked
+      window.location.href = "about.html";
+    });
+  });
+  
--- script.js
+++ script.js
@@ -1,13 +1,14 @@
+/*
 var gifs = document.getElementsByClassName("gif");
 var displayButton = document.getElementById("displayButton");
 
-/* Function that hides gifs*/
+//Function that hides gifs
 function hideGifs() {
   for (var i = 0; i < gifs.length; i++) {
     gifs[i].style.display = "none";
   }
 }
-/* Function that shows the gif selected in the list*/
+/* Function that shows the gif selected in the list
 function showSelectedGif() {
   var selectedGif = document.getElementById(document.getElementById("gifSelector").value);
   hideGifs();
@@ -28,42 +29,3 @@ function toggleNav() {
     nav.className = "topnav";
   }
 }
-
-
-
-/*Buttons code*/
-// Get the button element
-const learnMoreButton = document.querySelector('.learn-more');
-
-// Add click event listener to the button
-
-learnMoreButton.addEventListener('click', () => {
-  // Redirect to the desired HTML file
-  window.location.href = 'about.html';
-});
-
-// Get the buttons by their IDs
-const redirectButton = document.getElementById("redirectButton");
-const explanationButton = document.getElementById("explanationButton");
-const redirectButton1 = document.getElementById("redirectButton1");
-const explanationButton1 = document.getElementById("explanationButton1");
-
-// Add click event listeners to the buttons
-redirectButton.addEventListener("click", () => {
-  // Redirect to the desired page when the button is clicked
-  window.location.href = "GoogleMaps.html";
-});
-redirectButton1.addEventListener("click", () => {
-  // Redirect to the desired page when the button is clicked
-  window.location.href = "hazard.html";
-});
-
-explanationButton.addEventListener("click", () => {
-  // Redirect to another page when the button is clicked
-  window.location.href = "about.html";
-});
-explanationButton1.addEventListener("click", () => {
-  // Redirect to another page when the button is clicked
-  window.location.href = "about.html";
-});
-

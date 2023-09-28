b549875dbb8860fa31b66f3cf44ffc34c9f9f198
Alba <albaclosat@gmail.com>
2023-09-28 16:28:37 -0400

    Need it changes

--- execute_batch.php
+++
@@ -1,4 +0,0 @@
-<?php
-$output = shell_exec("C:\Users\Rojano\Documents\GitHub\Flooding\run_python.bat");
-echo nl2br($output);
-?>
--- run_python.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-python C:\Users\Rojano\Documents\GitHub\Flooding\bat_run2.py

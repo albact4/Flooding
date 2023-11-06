3eadea68880541c7a3246d95f13bdf8ea1a3571c
Alba <albaclosat@gmail.com>
2023-11-06 10:39:12 -0500

    responsive menu js

--- script.js
+++ script.js
@@ -9,3 +9,12 @@ function toggleNav() {
     nav.className = "topnav";
   }
 }
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
